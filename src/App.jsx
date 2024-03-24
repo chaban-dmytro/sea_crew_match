2d6e33d281631e2bd795b1d73365c709cb40345f
chaban-dmytro <dimach@ukr.net>
2024-03-24 21:14:06 +0100

    change json

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,12 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<WelcomePage />} />
+        <Route path="" element={<WelcomePage />} />
         <Route path="/signup" element={<SingUpPage />} />
         <Route path="/login" element={<LogInPage />} />
         <Route path="/projects" element={<MainPage />} />
         <Route path="/projects/:id" element={<div>Ptoject</div>} />
       </Routes>
-      {/* <MainPage /> */}
     </div>
   );
 }
